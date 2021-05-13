99ed5f26a7c2955a7c4a442ce86be6363a3fec73
Mateusz Darul <77622126+Mateusz-DevCpp@users.noreply.github.com>
2021-05-13 11:53:13 +0200

    init wzorcow

---
+++ Wzorce/Adapter/src/main/java/io/adapter/Main.java
@@ -0,0 +1,9 @@
+package io.adapter;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
---
+++ Wzorce/Budowniczy/src/main/java/io/budowniczy/Main.java
@@ -0,0 +1,10 @@
+package io.budowniczy;
+
+public class Main 
+{
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+}
