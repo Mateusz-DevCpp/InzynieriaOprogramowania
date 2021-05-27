97fb9b6935a6279fb2ba2d122264e23f487aa5f1
Alicja <229875@edu.p.lodz.pl>
2021-05-27 21:49:54 +0200

    Prototyp cz2

--- Wzorce/Prototyp/src/main/java/io/prototyp/Animal.java
+++ Wzorce/Prototyp/src/main/java/io/prototyp/Animal.java
@@ -2,7 +2,7 @@ package io.prototyp;
 
 //Prototype
 
-//Clonable = it is legal for that method to make a field-for-field copy of instances of that class.
+//Cloneable = it is legal for that method to make a field-for-field copy of instances of that class.
 // Invoking Object's clone method on an instance that does not implement the Cloneable interface
 // results in the exception CloneNotSupportedException being thrown.
 public interface Animal extends Cloneable{
