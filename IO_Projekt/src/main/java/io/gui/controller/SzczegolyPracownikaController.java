90a5120d3539d6a04532cdb62c0663c461557277
zuziakowalska <72947214+zuziakowalska@users.noreply.github.com>
2021-06-14 19:05:09 +0200

    gui model
    GUI xd

---
+++ IO_Projekt/src/main/java/io/gui/controller/SzczegolyPracownikaController.java
@@ -0,0 +1,84 @@
+package io.gui.controller;
+
+import io.gui.Main;
+import io.gui.Window;
+import io.model.system.Karta;
+import io.model.system.Lokal;
+import io.model.system.Lokalizacja;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class SzczegolyPracownikaController 
+{
+    private Karta karta_pracownika;
+    
+    @FXML
+    public void initialize()
+    {
+        tf_id.setText(Integer.toString(WyswietlPracownikowController.wybrany_pracownik.getId()));
+        tf_imie.setText(WyswietlPracownikowController.wybrany_pracownik.getImie());
+        tf_nazwisko.setText(WyswietlPracownikowController.wybrany_pracownik.getNazwisko());
+        tf_ulica.setText(WyswietlPracownikowController.wybrany_pracownik.getAdres().getUlica());
+        tf_mieszkanie.setText(WyswietlPracownikowController.wybrany_pracownik.getAdres().getMieszkanie());
+        tf_kod_pocztowy.setText(WyswietlPracownikowController.wybrany_pracownik.getAdres().getKodPocztowy());
+        tf_miasto.setText(WyswietlPracownikowController.wybrany_pracownik.getAdres().getMiasto());
+        tf_email_pri.setText(WyswietlPracownikowController.wybrany_pracownik.getKontaktPrywatny().getEmail());
+        tf_nr_tel_pri.setText(WyswietlPracownikowController.wybrany_pracownik.getKontaktPrywatny().getNrTelefonu());
+        tf_email_pub.setText(WyswietlPracownikowController.wybrany_pracownik.getKontaktSluzbowy().getEmail());
+        tf_nr_tel_pub.setText(WyswietlPracownikowController.wybrany_pracownik.getKontaktSluzbowy().getNrTelefonu());
+    
+        karta_pracownika = Main.manager_kart.getByCardID(WyswietlPracownikowController.wybrany_pracownik.getKarta());
+        
+        if(WyswietlPracownikowController.wybrany_pracownik.getKarta() != -1)
+        {
+            for(int i=0; i<karta_pracownika.getHistoria().size(); i++)
+            {
+                System.err.println("---\n");
+                lv_his_lokalizacji.getItems().add(karta_pracownika.getHistoria().get(i));
+            }
+            System.err.println(karta_pracownika.getHistoria().get(0).toString());
+        }
+        else
+        {
+            lv_his_lokalizacji.getItems().add(
+            new Lokalizacja(
+                    new Lokal("ERROR - Brak karty", 0, 0, ""), 
+                    false)
+            );
+        }
+    }
+    
+    @FXML
+    public void mainMenu() throws IOException
+    {
+        Window.setRoot("MainWindow");
+    }
+    
+    @FXML
+    public void menuDodajPracownika() throws IOException
+    {
+        Window.setRoot("DodajPracownikaWindow");
+    }
+    
+    @FXML
+    public void menuWyswietlPracownikow() throws IOException
+    {
+        Window.setRoot("WyswietlPracownikowWindow");
+    }
+    
+    @FXML private TextField tf_id;
+    @FXML private TextField tf_imie;
+    @FXML private TextField tf_nazwisko;
+    @FXML private TextField tf_miasto;
+    @FXML private TextField tf_ulica;
+    @FXML private TextField tf_mieszkanie;
+    @FXML private TextField tf_kod_pocztowy;
+    @FXML private TextField tf_email_pri;
+    @FXML private TextField tf_nr_tel_pri;
+    @FXML private TextField tf_email_pub;
+    @FXML private TextField tf_nr_tel_pub;
+    
+    @FXML private ListView<Lokalizacja> lv_his_lokalizacji;
+}
